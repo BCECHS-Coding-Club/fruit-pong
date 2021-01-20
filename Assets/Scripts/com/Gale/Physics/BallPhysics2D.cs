065236861c38a5030f5afa7ed8b4612cb103fa26
Tristan Zander <62917486+tristan-zander@users.noreply.github.com>
2021-01-20 13:44:45 -0500

    Fixed raycast bug in BallPhysics.
    The angle that the rays should have been cast at weren't being converted to radians.

--- Assets/Scripts/com/Gale/Physics/BallPhysics2D.cs
+++ Assets/Scripts/com/Gale/Physics/BallPhysics2D.cs
@@ -43,24 +43,26 @@ namespace com.Gale.Physics
 
         private void OnDrawGizmos()
         {
+            //
+            //  This is basically a repeat of this.CastRays for debugging.
+            //
             var rayTotal = 2 * rayCount;
             var position = transform.position;
 
-            var velocityAngle = Vector2.Angle(Vector2.right, _velocity);
+            var velocityAngle = Vector2.Angle(Vector2.right, _velocity) * Mathf.Deg2Rad;
            
             Gizmos.color = Color.green;
-            Gizmos.DrawLine(position, new Vector3(position.x + Mathf.Acos(_velocity.normalized.x) * collider.radius * 2,
-                position.y + Mathf.Asin(_velocity.normalized.y) * collider.radius * 2));
-            
-            var perpendicularAngle = Vector2.Angle(Vector2.right, _velocity) + Mathf.PI / 2;
+            Gizmos.DrawLine(position, new Vector3(position.x + Mathf.Cos(velocityAngle) * collider.radius * 2,
+                position.y + Mathf.Sin(velocityAngle) * collider.radius * 2));
+ 
+            // TODO switch to Vector2.Perpendicular
+            var perpendicularAngle = Vector2.Angle(Vector2.right, _velocity) * Mathf.Deg2Rad + Mathf.PI / 2;
             Gizmos.DrawLine(position, new Vector3(position.x + Mathf.Cos(perpendicularAngle) * collider.radius,
                 position.y + Mathf.Sin(perpendicularAngle) * collider.radius));
             
-            Assert.AreEqual(velocityAngle + Mathf.PI / 2, perpendicularAngle );
-            
             for (var i = 0; i <= rayTotal; i++)
             {
-                var radDirection = Vector2.Angle(Vector2.up, _velocity);
+                var radDirection = Vector2.Angle(Vector2.up, _velocity) * Mathf.Deg2Rad;
                 var angle =  (Mathf.PI / rayTotal * i) - radDirection;
 
                 
@@ -74,29 +76,26 @@ namespace com.Gale.Physics
         }
 
         // TODO radDirection should be phased out for Vector2.Direction
-        private List<RaycastHit2D> CastRays(float radDirection)
+        private float CastRays(float radDirection)
         {
+            // Equation for raycast origin:
+            // Vector2(cos(rayAngle - velocityAngle), sin(rayAngle - velocityAngle)) * radius + skin
             var rayTotal = 2 * rayCount + 1;
             var position = transform.position;
 
-            var ret = new List<RaycastHit2D>(rayTotal);
+            var hitDistance = -1.0f;
             
             for (var i = 1; i <= rayTotal; i++)
             {
-                var angle =  Mathf.PI / i;
+                var rayAngle =  Mathf.PI / i;
 
-                var startPoint = new Vector2(Mathf.Cos(angle + radDirection) * collider.radius * skinWidth + position.x,
-                    Mathf.Sin(angle - radDirection) * collider.radius * skinWidth + position.y);
+                var startPoint = new Vector2(Mathf.Cos(rayAngle + radDirection) * collider.radius + skinWidth + position.x,
+                    Mathf.Sin(rayAngle - radDirection) * collider.radius + skinWidth + position.y);
 
                 var ray = Physics2D.Raycast(startPoint, _velocity.normalized, _velocity.magnitude, collisionLayer);
-
-                if (ray)
-                {
-                    ret.Add(ray);
-                }
             }
 
-            return ret;
+            return hitDistance;
         }
     }
 }
